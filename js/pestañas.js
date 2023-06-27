99bdb5c03ecc5f5a7da83ab13fd6a5a62656855a
dorismeza <dorismeza56@gmail.com>
2023-06-26 20:14:22 -0600

    costa-rica,panama

--- js/pestañas.js
+++ js/pestañas.js
@@ -1,20 +1,15 @@
- // Función JavaScript para manejar las pestañas
- function openTab(tabName) {
-    var i, tabcontent, tablinks;
-  
-    // Ocultar todos los contenidos
-    tabcontent = document.getElementsByClassName("content");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    // Remover la clase 'active' de todas las pestañas
-    tablinks = document.getElementsByClassName("tab");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].classList.remove("active");
-    }
-  
-    // Mostrar el contenido de la pestaña seleccionada y agregar la clase 'active' a la pestaña
-    document.getElementById(tabName).style.display = "block";
-    document.getElementById(tabName + "-tab").classList.add("active");
-  }
+document.addEventListener("DOMContentLoaded", function() {
+  $('ul.tabs li a:first').addClass('active');
+  $('.secciones article').hide();
+  $('.secciones article:first').show();
+
+  $('ul.tabs li a').click(function() {
+      $('ul.tabs li a').removeClass('active');
+      $(this).addClass('active');
+      $('.secciones article').hide();
+
+      var activeTab = $(this).attr('href');
+      $(activeTab).show();
+      return false;
+  });
+});
